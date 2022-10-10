34f7ba1cf52ade7b2efc3f8acc6a48a5e2ba56e0
Ahmed <a.sabrymazroua@hotmail.com>
2022-10-10 15:36:30 +0200

    adding main file to the repo

---
+++ main.py
@@ -0,0 +1,1 @@
+# -*- coding: utf-8 -*-"""Created on Mon Oct 10 15:17:13 2022@author: Ahmed"""from pix2pix import Pix2Pixfrom testing_function import random_testif __name__ == "__main__":        gan = Pix2Pix()    # gan.train(epochs=200, batch_size=1, sample_interval=200)    gan.train(epochs=3000, batch_size=1, sample_interval=200)
